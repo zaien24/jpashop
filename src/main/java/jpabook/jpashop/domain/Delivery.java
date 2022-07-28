1b25893a31ee554f4f84977d2981f7aeb0d27afd
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-07-29 00:29:44 +0900

    엔티티 설계시 주의점 지연로딩 처리

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -15,7 +15,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 
     @Embedded
