b04f643b3d2ac95ccee7f8132a9d884e676dd08e
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-07-30 00:49:10 +0900

    회원 기능 테스트 중복 회원 예외 테스트

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -28,7 +28,6 @@ public class MemberServiceTest {
     EntityManager em;
 
     @Test
-    @Rollback(false)
     public void 회원가입() throws Exception {
         //given
         Member member = new Member();
@@ -45,11 +44,24 @@ public class MemberServiceTest {
     @Test
     public void 중복_회원_예외() throws Exception {
         //given
+        Member member1 = new Member();
+        member1.setName("kim1");
+
+        Member member2 = new Member();
+        member2.setName("kim2");
         
                 
         //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);    //예외가 발생해야 한다!!!
+        } catch (IllegalStateException e) {
+            return;
+        }
+
         
         //then
+        fail("예외가 발생해야 한다.");
     }
 
 }
