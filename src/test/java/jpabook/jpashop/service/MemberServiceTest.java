8895780a9ac3f28ac61c9af5f6a9874126e47a6c
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-07-30 00:41:56 +0900

    회원 기능 테스트
    insert  확인

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,8 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.Assert.*;
 
@@ -18,11 +21,14 @@ public class MemberServiceTest {
 
     @Autowired
     MemberService memberService;
-
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    EntityManager em;
+
     @Test
+
     public void 회원가입() throws Exception {
         //given
         Member member = new Member();
@@ -32,6 +38,7 @@ public class MemberServiceTest {
         Long savedId = memberService.join(member);
         
         //then
+        em.flush();
         assertEquals(member, memberRepository.findOne(savedId));
     }
     
