a34ebf07adfce72bb824a07a3cffdd9e6b7904f9
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-08-07 14:34:36 +0900

    섹션1. API개발기본 회원조회 API 3

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
+import java.util.stream.Collectors;
 
 //@Controller @ResponseBody
 @RestController
@@ -22,6 +23,29 @@ public class MemberApiController {
     @GetMapping("/api/v1/members")
     public List<Member> membersV1() {
         return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(),collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
     }
 
     @PostMapping("/api/v1/members")
@@ -78,6 +102,7 @@ public class MemberApiController {
             this.id = id;
         }
     }
+
 
 
 }
