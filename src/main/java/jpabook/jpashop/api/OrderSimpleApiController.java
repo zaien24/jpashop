a3d2a3a9f91d14466ed2c9275d0eb6b60c34c737
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-08-10 22:04:24 +0900

    섹션3. API개발고급 - 지연로딩과 조회 성능 최적화 간단한 주문 조회 V1: 엔티티를 직접 노출3

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -24,6 +24,12 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
+
+        for (Order order : all) {
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress(); // Lazy 강제 초기화
+        }
+
         return all;
     }
 
