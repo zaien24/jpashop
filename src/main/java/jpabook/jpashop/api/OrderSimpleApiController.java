bd7bfa757528e76c6b8ae3ce92872841ecf9b42e
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-08-12 22:18:06 +0900

    섹션3. API개발고급 - 지연로딩과 조회 성능 최적화 간단한 주문 조회 V3:엔티티를 DTO로 변환 - 페치 조인 최적화 간단한 주문 조회 V3:JPA에서 DTO로 바로 조회

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.OrderSimpleQueryDto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,6 +47,21 @@ public class OrderSimpleApiController {
                 .map(SimpleOrderDto::new)
                 .collect(toList());
     }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+         List<Order> orders = orderRepository.findWithMemberDelivery();
+         List<SimpleOrderDto> result = orders.stream()
+                 .map(o -> new SimpleOrderDto(o))
+                 .collect(Collectors.toList());
+
+         return result;
+    }
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderRepository.findOrderDtos();
+    }
+
 
     @Data
     static class SimpleOrderDto {
