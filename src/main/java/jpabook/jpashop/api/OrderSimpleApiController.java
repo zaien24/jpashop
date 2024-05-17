b69ba3280bfc87c49155abe5736306e8165ad19c
yuyuhi <zaien24@naver.com>
2024-05-17 12:58:22 +0900

    섹션3 간단한 주문 조회 V1: 엔티티를 직접 노출  _2

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -25,6 +25,11 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-order")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        
+        for (Order order : all) {
+            order.getMember().getName();        // Lazy 강제 초기화
+            order.getDelivery().getAddress();   // Lazy 강제 초기화
+        }
         return all;
     }
 
