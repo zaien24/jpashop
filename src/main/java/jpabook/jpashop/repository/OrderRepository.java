83ae3ce43e9c4f4089dbd4f047f3f03b1f75985b
yuyuhi <zaien24@naver.com>
2024-05-21 08:31:02 +0900

    섹션4 주문 조회 V1 : 엔티티 직접 노출

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -21,8 +22,31 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAllByString(OrderSearch orderSearch) {
-//    }
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += "where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+
+            }
+        }
+    }
 
 //    public List<Order> findAll(OrderSearch orderSearch) {
 //
